3f63e243fb0b9632bd2918b73a2be710d4c6fc88
NardiniA <techteacher.pershore@gmail.com>
2024-02-05 14:00:12 +0000

    Add icons
    Added the icons for J Rigg. Also removed some meta data which was causing errors.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,7 @@ export async function generateMetadata(): Promise<Metadata> {
   const newImage = generateMetaImage(metadata?.ogImage as Media);
 
   return {
+    metadataBase: new URL(settings?.siteURL),
     title: {
       default: `${metadata?.title} ${metadata?.suffix}`,
       template: `%s ${metadata?.suffix}`,
@@ -74,7 +75,6 @@ export async function generateMetadata(): Promise<Metadata> {
         name: metadata?.author,
       },
     ],
-    metadataBase: new URL(settings?.siteURL),
     creator: metadata?.author,
     publisher: metadata?.author,
     keywords: metadata?.keywords.map(({ name }: { name: string }) => name),
@@ -99,7 +99,100 @@ export async function generateMetadata(): Promise<Metadata> {
       follow: true,
       nocache: true,
     },
-    // TODO: Add icons list
+    icons: {
+      icon: [
+        {
+          url: "/favicon-16x16.png",
+          type: "image/png",
+          sizes: "16x16",
+        },
+        {
+          url: "/favicon-32x32.png",
+          type: "image/png",
+          sizes: "32x32",
+        },
+        {
+          url: "/favicon-96x96.png",
+          type: "image/png",
+          sizes: "96x96",
+        },
+        {
+          url: "/ms-icon-144x144.png",
+          type: "image/png",
+          sizes: "144x144",
+        },
+        {
+          url: "/android-icon-192x192.png",
+          type: "image/png",
+          sizes: "192x192",
+        },
+      ],
+      shortcut: [
+        {
+          url: "/favicon-16x16.png",
+          type: "image/png",
+          sizes: "16x16",
+        },
+        {
+          url: "/favicon-32x32.png",
+          type: "image/png",
+          sizes: "32x32",
+        },
+        {
+          url: "/favicon-96x96.png",
+          type: "image/png",
+          sizes: "96x96",
+        },
+      ],
+      apple: [
+        {
+          url: "/apple-icon-57x57.png",
+          type: "image/png",
+          sizes: "57x57",
+        },
+        {
+          url: "/apple-icon-60x60.png",
+          type: "image/png",
+          sizes: "60x60",
+        },
+        {
+          url: "/apple-icon-72x72.png",
+          type: "image/png",
+          sizes: "72x72",
+        },
+        {
+          url: "/apple-icon-76x76.png",
+          type: "image/png",
+          sizes: "76x76",
+        },
+        {
+          url: "/apple-icon-114x114.png",
+          type: "image/png",
+          sizes: "114x114",
+        },
+        {
+          url: "/apple-icon-120x120.png",
+          type: "image/png",
+          sizes: "120x120",
+        },
+        {
+          url: "/apple-icon-144x144.png",
+          type: "image/png",
+          sizes: "144x144",
+        },
+        {
+          url: "/apple-icon-152x152.png",
+          type: "image/png",
+          sizes: "152x152",
+        },
+        {
+          url: "/apple-touch-icon.png",
+          type: "image/png",
+          sizes: "180x180",
+        },
+      ],
+    },
+    // // TODO: Add icons list
     twitter: {
       card: "summary_large_image",
       title: metadata?.title,
@@ -116,7 +209,6 @@ export async function generateMetadata(): Promise<Metadata> {
         should_fallback: true,
       },
     },
-    assets: [newImage?.url?.replace(newImage?.filename as string, "") as string],
     category: "construction",
   };
 }
