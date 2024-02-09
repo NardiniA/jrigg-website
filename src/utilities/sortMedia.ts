8abdcad15e2baf4ca66fc067b45e135a23ee4525
NardiniA <techteacher.pershore@gmail.com>
2024-02-09 11:17:00 +0000

    Update sortMedia function
    Fixed errors and sorting function for sortMedia field.

--- src/utilities/sortMedia.ts
+++ src/utilities/sortMedia.ts
@@ -9,19 +9,26 @@ type FilenameInfo = {
 type SortMedia = (media: (Media | GalleryMedia)[]) => (Media | GalleryMedia)[]
 
 function parseFilename(filename: string): FilenameInfo {
-  const match = filename.match(/^(.+)-(\d+)\.(.*)$/);
-  return match ? {
-    prefix: match[1],
-    number: parseInt(match[2]),
+  const number = filename?.match(/\d+/)?.at(-1);
+  const project = filename?.split(/\d+/)[0]?.replace(/-/g, " ");
+
+  return (project && number) ? {
+    prefix: project,
+    number: parseInt(number),
   } : null;
 }
 
-export const sortMedia = (media: Media[]): Media[] => {
+export const sortMedia: SortMedia = (media) => {
   return media?.sort((a, b) => {
     const parsedA = parseFilename(a?.filename as string);
     const parsedB = parseFilename(b?.filename as string);
 
-    if (!parsedA || !parsedB) throw new Error("Invalid filename format");
+    
+    if (!parsedA || !parsedB) {
+      console.log(parsedA);
+      console.log(parsedB);
+      throw new Error("Invalid filename format");
+    }
 
     const prefixComparison = parsedA.prefix.localeCompare(parsedB.prefix);
     if (prefixComparison !== 0) return prefixComparison;
